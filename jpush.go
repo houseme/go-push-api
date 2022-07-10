6d620328f95790da1f0faa32704b25ff3b75dbe7
houseme <housemecn@gmail.com>
2022-07-10 10:56:30 +0800

    feat: create jpush send sms

--- jpush.go
+++ jpush.go
@@ -1,1 +1,35 @@
 package pushapi
+
+import (
+	"context"
+
+	"github.com/houseme/go-push-api/internal/jums"
+)
+
+// Jums 推送客户端
+type Jums struct {
+	ctx    context.Context
+	client *jums.Jums
+}
+
+// SendSms 发送短信
+func (j *Jums) SendSms(tempID int, phone, code, signID string) error {
+	var adu = []string{phone}
+	return j.client.Message().To(jums.ToSms("sms", adu)).Content(jums.MsgSms(signID, tempID, map[string]any{"code": code})).Send()
+}
+
+// NewClient 创建推送客户端
+func NewClient(ctx context.Context, key, secret string) *Jums {
+	return &Jums{
+		ctx: ctx,
+		client: jums.New(jums.Config{
+			Key:    key,
+			Secret: secret,
+		}),
+	}
+}
+
+// SendPush 发送推送
+func (j *Jums) SendPush(ctx context.Context, client *jums.Jums, title, content, url string, aud []string) error {
+	return nil
+}
