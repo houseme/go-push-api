5689c7767ed2c323d93832bb4c8b230c552ed7e1
houseme <housemecn@gmail.com>
2022-07-15 10:15:02 +0800

    feat: create send email

--- jpush.go
+++ jpush.go
@@ -13,7 +13,7 @@ type Jums struct {
 }
 
 // SendSms 发送短信
-func (j *Jums) SendSms(tempID int, phone, code, signID string) error {
+func (j *Jums) SendSms(ctx context.Context, tempID int, phone, code, signID string) error {
 	var adu = []string{phone}
 	return j.client.Message().To(jums.ToSms("sms", adu)).Content(jums.MsgSms(signID, tempID, map[string]interface{}{"code": code})).Send()
 }
@@ -32,4 +32,10 @@ func NewClient(ctx context.Context, key, secret string) *Jums {
 // SendPush 发送推送
 func (j *Jums) SendPush(ctx context.Context, client *jums.Jums, title, content, url string, aud []string) error {
 	return nil
+}
+
+// SendEmail 发送邮件
+func (j *Jums) SendEmail(ctx context.Context, tempID int, email, code, signID string) error {
+	var adu = []string{email}
+	return j.client.Message().To(jums.ToEmail("sms", adu)).Content(jums.MsgSms(signID, tempID, map[string]interface{}{"code": code})).Send()
 }
